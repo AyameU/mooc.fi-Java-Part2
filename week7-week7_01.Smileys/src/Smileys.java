d61406ef99df7ce36f788a38cb65be9d27e88104
AyameU <ayamekea@gmail.com>
2020-03-08 20:59:54 -0500

    7.01 Solution. Not sure if this is the best way to structure this program.

--- week7-week7_01.Smileys/src/Smileys.java
+++ week7-week7_01.Smileys/src/Smileys.java
@@ -3,9 +3,42 @@ public class Smileys {
 
     public static void main(String[] args) {
         // Test your method at least with the following
-        // printWithSmileys("Method");
-        // printWithSmileys("Beerbottle");
-        // printWithSmileys("Interface");
+        printWithSmileys("\\:D/");
+        printWithSmileys("Method");
+        printWithSmileys("Beerbottle");
+        printWithSmileys("Interface");
+    }
+
+    private static void printWithSmileys(String characterString) {
+        String smiley = ":)";
+
+        if(characterString.length() % 2 == 1) {
+            printFirstLastLine(characterString);
+            System.out.println(smiley + " " + characterString + "  " + smiley);
+            printFirstLastLine(characterString);
+        }
+        else {
+            printFirstLastLine(characterString);
+            System.out.println(smiley + " " + characterString + " " + smiley);
+            printFirstLastLine(characterString);
+        }
+    }
+
+    private static void printFirstLastLine(String characterString) {
+        String smiley = ":)";
+
+        if(characterString.length() % 2 == 1) {
+            for(int i = 0; i < (characterString.length() + 7)/2; i++) {
+                System.out.print(smiley);
+            }
+            System.out.print("\n");
+        }
+        else {
+            for(int i = 0; i < (characterString.length() + 6)/2; i++) {
+                System.out.print(smiley);
+            }
+            System.out.print("\n");
+        }
     }
 
 }
