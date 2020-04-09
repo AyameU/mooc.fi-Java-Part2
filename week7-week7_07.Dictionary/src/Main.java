f38340c03e0326ea4c38bfba0a661bb103218482
AyameU <ayamekea@gmail.com>
2020-04-08 23:37:03 -0500

    Not sure what I updated. Lol

--- week7-week7_07.Dictionary/src/Main.java
+++ week7-week7_07.Dictionary/src/Main.java
@@ -4,10 +4,17 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner reader = new Scanner(System.in);
-        Dictionary dict = new Dictionary();
+        String input = "add\n"  + "cheese\n" + "juusto\n" +
+                "add\n"  + "bier\n" + "olut\n" +
+                "add\n"  + "book\n" + "kirja\n" +
+                "add\n"  + "computer\n" + "tietokone\n" +
+                "add\n"  + "auto\n" + "car\n" +
+                "quit\n";
 
-        TextUserInterface ui = new TextUserInterface(reader, dict);
+        Scanner reader = new Scanner(input);
+        Dictionary dictionary = new Dictionary();
+
+        TextUserInterface ui = new TextUserInterface(reader, dictionary);
         ui.start();
     }
 }
