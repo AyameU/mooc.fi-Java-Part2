70b110f983ee0b76c9c6e238e3a9e78cb95dc844
AyameU <ayamekea@gmail.com>
2020-04-21 02:42:05 -0500

    7.08. Removed some unnecessary lines.

--- week7-week7_08.Airport/src/Flight.java
+++ week7-week7_08.Airport/src/Flight.java
@@ -1,12 +1,10 @@
 public class Flight {
     private Plane plane;
-    private String planeId;
     private String departureCode;
     private String destinationCode;
 
     public Flight(Plane plane, String departureCode, String destinationCode) {
         this.plane = plane;
-        this.planeId = plane.getId();
         this.departureCode = departureCode;
         this.destinationCode = destinationCode;
     }
