c6b59c34a3f86902bc63c0c05083370cac119893
AyameU <ayamekea@gmail.com>
2020-04-30 18:44:42 -0500

    8.11.3 & 8.11.4 Solutions.

--- week8-week8_11.BoxesAndThings/src/Box.java
+++ week8-week8_11.BoxesAndThings/src/Box.java
@@ -1,19 +1,16 @@
 import java.util.ArrayList;
 
-public class Box {
+public class Box implements ToBeStored {
     private double maxWeight;
     private ArrayList<ToBeStored> thingsToBeStored = new ArrayList<ToBeStored>();
-    private double currentWeight;
 
     public Box(double maxWeight) {
         this.maxWeight = maxWeight;
-        this.currentWeight = 0;
     }
 
     public void add(ToBeStored t) {
-        if(t.weight() + this.currentWeight <= this.maxWeight) {
+        if(t.weight() + weight() <= this.maxWeight) {
             thingsToBeStored.add(t);
-            this.currentWeight += t.weight();
         }
     }
 
@@ -21,7 +18,18 @@ public class Box {
         return thingsToBeStored.size();
     }
 
+    @Override
+    public double weight() {
+        double weight = 0;
+
+        for(ToBeStored t : thingsToBeStored) {
+            weight += t.weight();
+        }
+        return weight;
+    }
+
+    @Override
     public String toString() {
-        return "Box: " + numOfThings() + " things, total weight " + this.currentWeight + " kg";
+        return "Box: " + numOfThings() + " things, total weight " + weight() + " kg";
     }
 }
--- week8-week8_11.BoxesAndThings/src/Main.java
+++ week8-week8_11.BoxesAndThings/src/Main.java
@@ -11,5 +11,21 @@ public class Main {
         box.add( new CD("Rendezvous Park", "Closer to Being Here", 2012) );
 
         System.out.println( box );
+
+        Box box2 = new Box(9);
+
+        box2.add( new CD("Pink Floyd", "Dark Side of the Moon", 1973) );
+        box2.add( new CD("Wigwam", "Nuclear Nightclub", 1975) );
+        box2.add( new CD("Rendezvous Park", "Closer to Being Here", 2012) );
+
+        System.out.println(box2);
+
+        Box box3 = new Box(8);
+
+        box2.add(new Book("Jane Austen", "Pride and Prejudice", 2.5));
+
+        box3.add(box2);
+
+        System.out.println(box3);
     }
 }
