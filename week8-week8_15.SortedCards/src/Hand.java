5647871effbe3874a4dda284e1ea263a4f920d43
AyameU <ayamekea@gmail.com>
2020-05-01 21:47:43 -0500

    8.15.3 solution

--- week8-week8_15.SortedCards/src/Hand.java
+++ week8-week8_15.SortedCards/src/Hand.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Hand {
     private ArrayList<Card> hands;
@@ -15,5 +16,9 @@ public class Hand {
         for(Card hand : hands) {
             System.out.println(hand);
         }
+    }
+
+    public void sort() {
+        Collections.sort(this.hands);
     }
 }
--- week8-week8_15.SortedCards/src/Main.java
+++ week8-week8_15.SortedCards/src/Main.java
@@ -10,9 +10,15 @@ public class Main {
         System.out.println(second);
         System.out.println(third);
 
+        System.out.println();
+        System.out.println("-- 8.15.1 --");
+
         System.out.println(second.compareTo(first));
         System.out.println(third.compareTo(second));
         System.out.println(first.compareTo(third));
+
+        System.out.println();
+        System.out.println("-- 8.15.2 --");
 
         Hand hand = new Hand();
 
@@ -22,5 +28,18 @@ public class Main {
         hand.add( new Card(2, Card.CLUBS) );
 
         hand.print();
+
+        System.out.println();
+        System.out.println("-- 8.15.3 --");
+
+        Hand hand2 = new Hand();
+
+        hand2.add( new Card(2, Card.SPADES) );
+        hand2.add( new Card(14, Card.CLUBS) );
+        hand2.add( new Card(12, Card.HEARTS) );
+        hand2.add( new Card(2, Card.CLUBS) );
+
+        hand2.sort();
+        hand2.print();
     }
 }
