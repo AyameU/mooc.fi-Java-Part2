1fdea87be5e44091e0edf12169912bd0ee25704c
AyameU <ayamekea@gmail.com>
2020-06-14 16:53:13 -0500

    9.19.2 Solution

--- week9-week9_19.MethodArgumentValidation/src/Main.java
+++ week9-week9_19.MethodArgumentValidation/src/Main.java
@@ -3,7 +3,11 @@ import validation.Calculator;
 
 public class Main {
     public static void main(String[] args) {
-        Person person = new Person(null, 25);
+        Person person = new Person("Angela", 25);
         System.out.println("Name: " + person.getName() + ", Age: " + person.getAge());
+
+        Calculator calculator = new Calculator();
+        System.out.println(calculator.multiplication(0));
+        System.out.println(calculator.binomialCoefficient(2, 1));
     }
 }
