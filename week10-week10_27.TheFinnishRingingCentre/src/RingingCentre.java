1145602c8c802758bbc865de692e9f07e0dd85fd
AyameU <ayamekea@gmail.com>
2020-08-17 15:32:57 -0500

    10.27.2 - Solution using HashMaps and Lists.

--- week10-week10_27.TheFinnishRingingCentre/src/RingingCentre.java
+++ week10-week10_27.TheFinnishRingingCentre/src/RingingCentre.java
@@ -1,26 +1,41 @@
 import java.util.HashMap;
 import java.util.List;
+import java.util.ArrayList;
 import java.util.Map;
 
 public class RingingCentre {
-    //variables
 
-    Map<Bird, List<String>> observationsPlaces;
-
-    //constructor
+    private Map<Bird, List<String>> observationsPlaces;
 
     public RingingCentre() {
         this.observationsPlaces = new HashMap<Bird, List<String>>();
     }
 
-    //methods
-
     public void observe(Bird bird, String place) {
-        // which records the observation
-        // and its place among the bird information
+        if(!this.observationsPlaces.containsKey(bird)) {
+            List<String> places = new ArrayList<String>();
+            places.add(place);
+            this.observationsPlaces.put(bird, places);
+        }
+        else if(this.observationsPlaces.containsKey(bird)) {
+            this.observationsPlaces.get(bird).add(place);
+        }
+
     }
 
     public void observations(Bird bird) {
-        // which prints all the observations of the parameter bird
+        if(observationsPlaces.containsKey(bird)) {
+            int numObservations = observationsPlaces.get(bird).size();
+            System.out.println(bird + " observations: " + numObservations);
+
+            // print all places observed
+            for(String p : observationsPlaces.get(bird)) {
+                System.out.println(p);
+            }
+
+        }
+        else {
+            System.out.println(bird + " observations: 0");
+        }
     }
 }
