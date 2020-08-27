43d9e0bb70d5ea68eb1fd598461b263cd5fabc6b
AyameU <ayamekea@gmail.com>
2020-08-27 16:06:52 -0500

    10.29.1 solution

---
+++ week10-week10_29.PersonAndTheirHeirs/src/Main.java
@@ -0,0 +1,11 @@
+import people.Person;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Person pekka = new Person("Pekka Mikkola", "Korsontie Street 1 031100 Vantaa");
+        Person esko = new Person("Esko Ukkonen", "Mannerheimintie Street 15 00100 Helsinki");
+        System.out.println(pekka);
+        System.out.println(esko);
+    }
+}
---
+++ week10-week10_29.PersonAndTheirHeirs/src/people/Person.java
@@ -0,0 +1,15 @@
+package people;
+
+public class Person {
+    private String name;
+    private String address;
+
+    public Person(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    public String toString() {
+        return this.name + "\n  " + this.address;
+    }
+}
