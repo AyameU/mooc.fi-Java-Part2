286c492811c9e6a173964ab0ffba4b9a97846c28
AyameU <ayamekea@gmail.com>
2020-10-25 17:23:07 -0500

    11.34.1 - Reading a file with the File and Scanner classes.

---
+++ week11-week11_34.FileManager/src/FileManager.java
@@ -0,0 +1,29 @@
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class FileManager {
+
+    private List<String> fileLines = new ArrayList<String>();
+
+    public List<String> read(String file) throws FileNotFoundException {
+        File f = new File(file);
+        Scanner scan = new Scanner(f);
+        while(scan.hasNextLine()) {
+            String line = scan.nextLine();
+            fileLines.add(line);
+        }
+        return fileLines;
+    }
+
+    public void save(String file, String text) throws IOException {
+    }
+
+    public void save(String file, List<String> texts) throws IOException {
+    }
+}
